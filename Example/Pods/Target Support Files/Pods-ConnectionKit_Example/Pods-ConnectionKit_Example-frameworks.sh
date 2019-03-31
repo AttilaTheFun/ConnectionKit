96b7f4304617645d7b1791ad4aef6dfde89e166f
Logan Shire <logan.shire@gmail.com>
2019-03-30 20:30:09 -0700

    Fix folder structure and bump version again

--- Example/Pods/Target Support Files/Pods-ConnectionKit_Example/Pods-ConnectionKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ConnectionKit_Example/Pods-ConnectionKit_Example-frameworks.sh
@@ -153,13 +153,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/ConnectionKit/ConnectionKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxAtomic/RxAtomic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/ConnectionKit/ConnectionKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxAtomic/RxAtomic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
