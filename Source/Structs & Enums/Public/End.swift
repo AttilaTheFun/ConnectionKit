f1708024b1875caad0357958042b8f2e7d553ea7
Logan Shire <logan.shire@gmail.com>
2019-05-24 20:37:04 -0700

    Update comments

--- Source/Structs & Enums/Public/End.swift
+++ Source/Structs & Enums/Public/End.swift
@@ -2,15 +2,21 @@
 public enum End: Hashable {
     /**
      The head end of the connection.
-     Typically for chronological data this is the most recent end.
+     For chronological data this is oldest end.
      Fetches from this end will use the `first` and `after` parameters per the relay cursor spec.
+
+     Example:
+     head -> [1, 2, 3, 4, 5]
      */
     case head
 
     /**
-     The tail (oldest) end of the connection.
-     Typically for chronological data this is the oldest recent end.
+     The tail end of the connection.
+     For chronological data this is the newest end.
      Fetches from this end will use the `last` and `before` parameters per the relay cursor spec.
+
+     Example:
+     [1, 2, 3, 4, 5] <- tail
      */
     case tail
 }
